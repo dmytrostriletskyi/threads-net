6e9d00e7891785f26adf32fc12f40f929d7ca01e
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2023-07-07 22:46:10 +0300

    Add instagrapi and pillow as requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements/project.txt', 'r') as f:
     requirements = f.read().splitlines()
 
 setup(
-    version='0.0.4',
+    version='0.0.5',
     name='threads-net',
     description='Threads (threads.net) Python API wrapper',
     long_description=long_description,
