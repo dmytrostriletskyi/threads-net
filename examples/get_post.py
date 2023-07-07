d0334e14d8559c5876967e32cc894fc93ab99227
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2023-07-07 22:23:46 +0300

    Add login and get user by id/name

--- examples/get_post.py
+++ examples/get_post.py
@@ -2,12 +2,15 @@
 Provide example of getting a post implementation.
 """
 import json
+import os
 
 from threads import Threads
 
 
 if __name__ == '__main__':
     threads = Threads()
-    post = threads.get_post(id=3141055616164096839)
+    threads.load_settings('session.json')
+    threads.login(os.environ.get('INSTAGRAM_USERNAME'), os.environ.get('INSTAGRAM_PASSWORD'))
 
+    post = threads.get_post(id=3141055616164096839)
     print(json.dumps(post, indent=4))
--- examples/get_user.py
+++ examples/get_user.py
@@ -1,6 +1,7 @@
 """
 Provide example of getting a user implementation.
 """
+import os
 import json
 
 from threads import Threads
@@ -8,6 +9,11 @@ from threads import Threads
 
 if __name__ == '__main__':
     threads = Threads()
-    user = threads.get_user(id=314216)
+    threads.load_settings('session.json')
+    threads.login(os.environ.get('INSTAGRAM_USERNAME'), os.environ.get('INSTAGRAM_PASSWORD'))
 
+    user = threads.get_user_by_username(username='zuck')
+    print(json.dumps(user, indent=4))
+
+    user = threads.get_user_by_id(id=314216)
     print(json.dumps(user, indent=4))
