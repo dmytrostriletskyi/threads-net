8e655d328accfd1290d79c67ce0722fa70e5eb58
FastFingertips <bugra.coskun@outlook.com>
2023-07-09 17:13:15 +0300

    update setup.py file: added file reading function, fixed URLs, improved formatting, and handled errors.

--- setup.py
+++ setup.py
@@ -3,19 +3,31 @@ Setup the package.
 """
 from setuptools import find_packages, setup
 
-with open('README.md', 'r', encoding='utf-8') as read_me:
-    long_description = read_me.read()
+def get_file_content(file_name: str, **kwargs) -> str:
+    try:
+        with open(file_name, **kwargs) as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        raise FileNotFoundError(f'File "{file_name}" not found.')
+    except UnicodeDecodeError:
+        raise UnicodeDecodeError(f'File "{file_name}" has wrong encoding.')
 
-with open('requirements/project.txt', 'r') as f:
-    requirements = f.read().splitlines()
+read_me = get_file_content('README.md', encoding='utf-8')
+requirements = get_file_content('requirements.txt').splitlines()
 
 setup(
     version='0.0.11',
     name='threads-net',
     description='Threads (threads.net) Python API wrapper',
-    long_description=long_description,
+    long_description=read_me,
     long_description_content_type='text/markdown',
-    url='https://github.com/dmytrostriletskyi/threads',
+    url='https://github.com/dmytrostriletskyi/threads-net',
+    project_urls={
+        'Issue Tracker': 'https://github.com/dmytrostriletskyi/threads-net/issues',
+        'Source Code': 'https://github.com/dmytrostriletskyi/threads-net',
+        'Download': 'https://github.com/dmytrostriletskyi/threads-net/tags'
+    },
     license='MIT',
     author='Dmytro Striletskyi',
     author_email='dmytro.striletskyi@gmail.com',
